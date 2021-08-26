b666e28c7b90a10969dfd4c47fe5cd0424f742ad
Long Nguyen <longnguyen2210@outlook.com>
2021-08-25 20:47:40 -0400

    my commit

---
+++ frontend/src/data.js
@@ -0,0 +1,64 @@
+const data = {
+	products: [
+		{
+			_id: "1",
+			name: "Nike Slim Shirt",
+			category: "Shirts",
+			image: "/images/p1.jpg",
+			price: 120,
+			brand: "Nike",
+			rating: 4.5,
+			numReviews: 10,
+			description: "high quality product",
+		},
+
+		{
+			_id: "2",
+			name: "Adidas Fit Shirt",
+			category: "Shirts",
+			image: "/images/p2.jpg",
+			price: 100,
+			brand: "Adidas",
+			rating: 4,
+			numReviews: 10,
+			description: "high quality product",
+		},
+
+		{
+			_id: "3",
+			name: "Dior Sauvage Shirt",
+			category: "Shirts",
+			image: "/images/p3.jpg",
+			price: 200,
+			brand: "Dior",
+			rating: 4.5,
+			numReviews: 10,
+			description: "high quality product",
+		},
+
+		{
+			_id: "4",
+			name: "Supreme short",
+			category: "Shorts",
+			image: "/images/p4.jpg",
+			price: 1200,
+			brand: "Supreme",
+			rating: 3,
+			numReviews: 10,
+			description: "high quality product",
+		},
+
+		{
+			_id: "5",
+			name: "Saint Laurent biker jacker",
+			category: "Jacket",
+			image: "/images/p5.jpg",
+			price: 120,
+			brand: "Saint Laurent",
+			rating: 1,
+			numReviews: 10,
+			description: "high quality product",
+		},
+	],
+};
+export default data;
